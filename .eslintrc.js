26ce4be4eb334dacef0c2a6576ab8c891261b53b
Kazuyuki <kadota.kazuyuki@gmail.com>
2022-12-23 13:38:01 -0300

    First commit and config

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    // 'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+    // project: './tsconfig.json'
+  },
+  plugins: ['@typescript-eslint', 'react'],
+  rules: {},
+}
---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxSingleQuote: false,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 80,
+  tabWidth: 2,
+  tabs: false,
+  semi: false,
+}
