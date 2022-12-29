b9aefc4ed29d1672af3afa0900195d117d9add0d
Kazu-Kadota <kadota.kazuyuki@gmail.com>
2022-12-29 08:21:23 -0300

    landing-page: create page 5

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -56,4 +56,12 @@ export default createGlobalStyle`
   a {
     text-decoration: none;
   }
+
+  & > a:link {
+    text-decoration: none;
+  }
+
+  & > a:visited {
+    text-decoration: none;
+  }
 `
