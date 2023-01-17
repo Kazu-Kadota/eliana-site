da813a44784cec7fcf2604ee877ad867c5eb05c1
Kazu-Kadota <kadota.kazuyuki@gmail.com>
2023-01-17 09:18:22 -0300

    feature: lint and ts script

--- src/pages/contact-me/styles.ts
+++ src/pages/contact-me/styles.ts
@@ -1,11 +1,4 @@
 import styled from 'styled-components'
 
-import config from 'src/styles/config'
-
-const colors = config.theme.colors
-const font_sizes = config.theme.font_sizes
-const screens = config.screens
-const variable = config.var
-
 export const Container = styled.div`
 `
