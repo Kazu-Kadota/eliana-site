3efb92b4b9fa6a5fad067b294bdbef0a9acc837f
Kazu-Kadota <kadota.kazuyuki@gmail.com>
2023-01-28 19:50:46 -0300

    find your forces: create page

---
+++ src/pages/courses/find-your-forces/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import BasicPages from 'src/components/common/basic-pages'
+
+import contents from './content'
+import { Container } from './styles'
+
+const FindYourForces: React.FC = () => {
+  return (
+  <Container>
+    {contents.map((content) => (
+      <BasicPages key={content.image} page_config={content} />
+    ))}
+  </Container>
+  )
+}
+
+export default FindYourForces
